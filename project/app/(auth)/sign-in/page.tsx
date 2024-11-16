36d0dd3a47db0a98b0fdc24193f7fc61d02b4197
Nalinaka <nalinaka@gmail.com>
2024-11-16 14:44:35 -0800

    Working on storage site

---
+++ project/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import AuthForm from "@components/AuthForm";
+
+const SignIn = () => <AuthForm type="Sign-in"/>
+  
+
+
+export default SignIn;
---
+++ project/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import AuthForm from "./components/AuthForm";
+
+const SignUp = () =>  <AuthForm type="Sign-in"/>
+
+export default SignUp;
