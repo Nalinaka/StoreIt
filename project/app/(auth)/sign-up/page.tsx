9d48c81e06cf39bbde70dd42228bcb1b801b955b
Nalinaka <nalinaka@gmail.com>
2024-11-16 18:34:42 -0800

    Completing the signin and signup form using next.js

--- project/app/(auth)/sign-up/page.tsx
+++ project/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import AuthForm from "./components/AuthForm";
 
-const SignUp = () =>  <AuthForm type="Sign-in"/>
+const SignUp = () =>  <AuthForm type="Sign-up"/>
 
 export default SignUp;
